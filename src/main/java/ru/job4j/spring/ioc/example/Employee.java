9e3217fe312d4f10b7ed6e58f387a2b4a45a88f1
Andrei Soloviev <hedg.r52@gmail.com>
2020-03-11 22:29:24 +0300

    1. Продемонстрировать различные виды создания spring beans.

---
+++ src/main/java/ru/job4j/spring/ioc/example/Employee.java
@@ -0,0 +1,12 @@
+package ru.job4j.spring.ioc.example;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Andrei Soloviev (hedg.r52@gmail.com)
+ * @version 1.0
+ * @since 11.03.2020
+ */
+@Component
+public class Employee {
+}
---
+++ src/main/java/ru/job4j/spring/ioc/example/User.java
@@ -0,0 +1,9 @@
+package ru.job4j.spring.ioc.example;
+
+/**
+ * @author Andrei Soloviev (hedg.r52@gmail.com)
+ * @version 1.0
+ * @since 11.03.2020
+ */
+public class User {
+}
